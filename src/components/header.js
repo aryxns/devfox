0a9c5b754352281672c3a6949665851830caf0ed
Aryan Sharma <52833999+aryxns@users.noreply.github.com>
2021-02-08 01:52:32 +0530

    Add files via upload

--- src/components/header.js
+++ src/components/header.js
@@ -9,10 +9,10 @@ function Header() {
       name: username
     }}}>DevFox</Link></h3>
       <div className="float-right">
-      <a href="/rules"><button className="mr-2 bg-gray-300 font-mono rounded-xl"><h4 className="p-2">Rules</h4></button></a>
-      <a href="/leaderboard"><button className="bg-green-300 font-mono rounded-xl mr-2"><h4 className="p-2">Leaderboard</h4></button></a>
-      <a href="/community"><button className="mr-2 bg-red-300 font-mono rounded-xl"><h4 className="p-2">Questions</h4></button></a>
-      <a href="/updates"><button className="mr-2 bg-blue-300 font-mono rounded-xl"><h4 className="p-2">Updates</h4></button></a>
+      <a href="/rules"><button className="mr-2 bg-gray-300 font-mono focus:outline-none focus:ring focus:border-blue-300 rounded-xl"><h4 className="p-2">Rules</h4></button></a>
+      <a href="/leaderboard"><button className="bg-green-300 focus:outline-none focus:ring focus:border-blue-300 font-mono rounded-xl mr-2"><h4 className="p-2">Leaderboard</h4></button></a>
+      <a href="/community"><button className="mr-2 bg-red-300 focus:outline-none focus:ring focus:border-blue-300 font-mono rounded-xl"><h4 className="p-2">Questions</h4></button></a>
+      <a href="/updates"><button className="mr-2 bg-blue-300 focus:outline-none focus:ring focus:border-blue-300 font-mono rounded-xl"><h4 className="p-2">Updates</h4></button></a>
       </div>
     </header>
     )
